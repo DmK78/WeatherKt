8eb356094f2aa8578d4a421c584ec67f7c50bb84
dmk <dimasan2006@gmail.com>
2020-03-16 13:35:04 +0500

    refactoring

--- app/src/main/java/ru/job4j/weatherkt/utils/Constants.java
+++ app/src/main/java/ru/job4j/weatherkt/utils/Constants.java
@@ -7,6 +7,7 @@ package ru.job4j.weatherkt.utils;
  */
 
 public class Constants {
+
     public static String key = "8f99535cdea446be868e707ba8062fc0";
     public static String units = "metric";
     public static String lang = "ru";
